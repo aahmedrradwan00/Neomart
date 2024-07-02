f7e6ac0b9adf81b15c22cfec0aefaa81130f8bc4
aahmedrradwan00 <ahmed.ibrahem1101@gmail.com>
2024-07-02 05:05:13 +0300

    updet

--- server/middleware/authentication.middleware.js
+++ server/middleware/authentication.middleware.js
@@ -5,19 +5,26 @@ const jwt = require('jsonwebtoken');
 
 const isAuthenticated = catchAsync(async (req, res, next) => {
     // extract payload
-    const token = req.headers.authorization.split(' ')[1];
-    if (!token) {return next(new Error('You are not logged in! Please log in to get access.', { cause: 401 }));}
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) token = req.headers.authorization.split(' ')[1];
+    if (!token) {
+        return next(new Error('You are not logged in! Please log in to get access.', { cause: 401 }));
+    }
     const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
     // check token in datebase
     const currentToken = await Token.findOne({ token, isValid: true });
-    if (!currentToken) {return next(new Error('token not found', { cause: 401 }));}
+    if (!currentToken) {
+        return next(new Error('token not found', { cause: 401 }));
+    }
     // check user
     const currentUser = await User.findById(decode.id);
-    if (!currentUser) {return next(new Error('The user belonging to this token not found', { cause: 401 }));}
+    if (!currentUser) {
+        return next(new Error('The user belonging to this token not found', { cause: 401 }));
+    }
     // get user
     req.user = currentUser;
     // call next middleware
     next();
 });
 
-module.exports =  isAuthenticated ;
+module.exports = isAuthenticated;
